a505efcbe697cc57f09a5de57989eb933da99398
fermer123 <fermer_525@mail.ru>
2023-08-02 13:00:33 +0500

    server

--- task/src/index.tsx
+++ task/src/index.tsx
@@ -1,6 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
-import App from './components/App';
+import App from './components/Pages/App';
 import GlobalStyle from './global';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
