4dd504a756c5fcc9a8866d604ca7f6ede3d1d6ce
fermer123 <fermer_525@mail.ru>
2023-08-06 14:11:44 +0500

    register

--- task/src/components/Pages/Register/Register.tsx
+++ task/src/components/Pages/Register/Register.tsx
@@ -1,7 +1,82 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
+import {Field, Form, Formik, FormikHelpers} from 'formik';
+import {useNavigate} from 'react-router-dom';
+import * as Yup from 'yup';
+
+import useLocalStorage from '@src/components/component/Hook/UseLocalStorage/useLocalStorage';
+import InputForm from '@src/components/component/InputForm/InputForm';
+import PostButton from '@src/components/component/PostButton/PostButton';
+import {IAuthData} from '@src/types';
+
+import register from '../../api/register/register';
+
+import {Auth, ErrorAlert} from './Register_style';
 
 const Register: FC = () => {
-  return <div>register</div>;
+  const [, setUser] = useLocalStorage('user', '');
+  const [errorLogin, setErrorLogin] = useState<string>('');
+  const push = useNavigate();
+
+  const validationSchema = Yup.object().shape({
+    password: Yup.string()
+      .min(3, 'Слишком короткий пароль')
+      .max(20, 'Слишком длинный пароль')
+      .required('Поле не должо быть пустым'),
+    email: Yup.string()
+      .email('Неверный email')
+      .required('Поле не должо быть пустым'),
+  });
+
+  const onSubmit = (values: IAuthData, actions: FormikHelpers<IAuthData>) => {
+    register({
+      email: values.email,
+      password: values.password,
+      setError: setErrorLogin,
+      push,
+      setUser,
+    });
+    actions.resetForm();
+    actions.setSubmitting(false);
+  };
+
+  return (
+    <Formik
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+      initialValues={{password: '', email: ''}}>
+      {({errors, touched, isSubmitting, dirty, handleSubmit}) => (
+        <Form>
+          <Auth>
+            <Field
+              error={errors.email}
+              touched={touched.email}
+              label='Введите email'
+              name='email'
+              component={InputForm}
+            />
+            <Field
+              error={errors.password}
+              touched={touched.password}
+              label='Введите пароль'
+              name='password'
+              component={InputForm}
+            />
+            <PostButton
+              disabled={
+                !dirty ||
+                isSubmitting ||
+                !!(errors.email && touched.email) ||
+                !!(errors.password && touched.password)
+              }
+              onSubmit={handleSubmit}
+              label='SIGN IN'
+            />
+            {!!errorLogin && <ErrorAlert label={errorLogin} color='error' />}
+          </Auth>
+        </Form>
+      )}
+    </Formik>
+  );
 };
 
 export default Register;
