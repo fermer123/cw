1ba443ba415105e11d343914ee61710dadd1329e
fermer123 <fermer_525@mail.ru>
2023-08-27 03:04:12 +0500

    global.d.ts

--- task/global.d.ts
+++ task/global.d.ts
@@ -1,1 +1,16 @@
 declare const isDev: boolean;
+
+// declare module '*.ttf';
+// declare module '*.png';
+// declare module '*.jpg';
+// declare module '*.gpeg';
+
+// declare module '*.svg?url' {
+//   const content: any;
+//   export default content;
+// }
+
+// declare module '*.svg' {
+//   const content: any;
+//   export default content;
+// }
