66ab5b67bbdf07e432f952825210f151adfe9621
fermer123 <fermer_525@mail.ru>
2023-09-04 01:08:39 +0500

    auth slice

--- task/src/store/api/authApi.ts
+++ task/src/store/api/authApi.ts
@@ -1,7 +1,27 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/dist/query/react';
+import {IAuthData} from '@src/app/types';
 
 // eslint-disable-next-line import/prefer-default-export
 export const authApi = createApi({
   reducerPath: 'authApi',
   baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:3000/'}),
+  tagTypes: ['auth'],
+  endpoints: (build) => ({
+    login: build.mutation({
+      query: (body: IAuthData) => ({
+        url: '/login',
+        method: 'POST',
+        body,
+      }),
+    }),
+    register: build.mutation({
+      query: (body: IAuthData) => ({
+        url: '/register',
+        method: 'POST',
+        body,
+      }),
+    }),
+  }),
 });
+
+export const {useLoginMutation, useRegisterMutation} = authApi;
