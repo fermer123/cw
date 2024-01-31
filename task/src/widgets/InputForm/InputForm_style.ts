be1a51c76cc412988ebfcd51cb02d2dc5a4d1e85
fermer123 <fermer_525@mail.ru>
2024-02-01 02:03:36 +0500

    smt

--- task/src/widgets/InputForm/InputForm_style.ts
+++ task/src/widgets/InputForm/InputForm_style.ts
@@ -8,16 +8,25 @@ const InputFormContent = styled(TextField)({
     color: colors.default,
     borderColor: colors.default,
   },
-  '& .Mui-focused .MuiOutlinedInput-notchedOutline ': {
-    borderColor: colors.default,
-    borderWidth: '0.5px ',
-  },
+
   '& .MuiOutlinedInput-notchedOutline': {
     color: colors.default,
     borderColor: colors.default,
   },
+  '&:hover .MuiOutlinedInput-notchedOutline': {
+    borderColor: colors.default, // или другой цвет по вашему выбору
+  },
+  '& .Mui-focused .MuiOutlinedInput-notchedOutline ': {
+    borderColor: colors.default,
+    borderWidth: '1px ',
+  },
   '& .MuiInputBase-input': {
     color: colors.default,
+  },
+
+  '& input:-webkit-autofill': {
+    '-webkit-text-fill-color': colors.default, // текст при авто заполнении
+    '-webkit-box-shadow': `0 0 0 100px ${colors.grey} inset`, // задний фон при автозаполнении
   },
 });
 export default InputFormContent;
